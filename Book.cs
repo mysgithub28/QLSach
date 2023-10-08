2a439a2c06d9a1f2e4e5814d969d2cfb3e10b6b5
mysgithub28 <phamhuynhtuyetmy57@gmail.com>
2023-10-08 08:24:34 +0700

    T1

---
+++ Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySach
+{
+    internal class Book
+    {
+        private string Title;
+        private string Author; 
+        private int Year;
+        private decimal Price;
+
+        public Book()
+        {
+            Title = Author = "Unavailable";
+            Price = 0;
+        }
+        public Book(string title, string author, int year, decimal price)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Year = year;
+            this.Price = price;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($" Tên sách: {Title} \n Tác giả: {Author} \n Năm xuất bản:  {Year} \n Giá sách: {Price} \n ");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Book book1 = new Book();
+            Book book2 = new Book("a", "b", 2020, 156.000m);
+            book1.DisplayInfo();
+            book2.DisplayInfo();
+            Console.ReadKey();
+        }
+    }
+}
